7c2fdae85910b7ab49e66a02f808da316170ae23
David Gokimmung <43725247+Davidgokimmung@users.noreply.github.com>
2019-10-01 22:26:25 +0530

    Update Davidgokimmung.cpp

--- Challenge/Davidgokimmung.cpp
+++ Challenge/Davidgokimmung.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 int main(){
     for(int i=1;i<=100;i++){
-        string x=i%3==0 && i%5==0 ? "Spider" : i%3==0 ? "Rat" : i%5==0 ? "Ghost" : "";
-        cout<<(x==""?to_string(i):x)<<endl;
+        cout<<(i%3==0 && i%5==0 ? "Spider" : i%3==0 ? "Rat" : i%5==0 ? "Ghost" : to_string(i))<<endl;
     }
 }
